20c97c8d84eabd68595378b66b5fbade3e39c8be
aseshghatani <aseshghatani4@gmail.com>
2025-04-12 15:01:17 +0530

    Your commit message for the new changes

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ require("./db/conn");
 const User=require("./models/user");
 
 const port = process.env.PORT || 3000;
-const static_path=path.join(__dirname, '../public')
+const static_path = path.join(__dirname, '../public');
+app.use(express.static(static_path));
 
 app.use(session({
   secret: "secretKey", // change this in production
@@ -17,7 +18,7 @@ app.use(session({
 
 app.use(express.urlencoded({ extended: false })); // for parsing form data
 app.use(express.json());
-app.use(express.static(static_path)); 
+
 app.set("view engine","hbs");
 app.get("/", (req, res) => {
   res.render("dashboard")
@@ -35,6 +36,12 @@ app.get("/user_dashboard",(req,res)=>{
   }
   res.render("user_dashboard", { user: req.session.user });
 });
+app.get("/menu",(req,res)=>{
+res.render("menu", { user: req.session.user })
+});
+app.get("/profile",(req,res)=>{
+  res.render("profile",req.session)
+})
 
 app.post("/register", async(req,res)=>{
   try{
@@ -55,9 +62,42 @@ app.post("/register", async(req,res)=>{
     res.status(400).send("Registration failed");
   }
 });
+app.get("/editProfile",(req,res)=>{
+  res.render("editProfile",req.session)
+})
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+      return res.send("Error logging out");
+    }
+    res.redirect("/");
+  });
+});
+app.post("/update_profile",async(req,res)=>{
+  const {name,email}=req.body;
+  const uid=req.session.user.id
+  try{
+    const updatedUser = await User.findByIdAndUpdate(
+        uid,
+      { name, email },
+      { new: true }
+    );
+
+    // Update session data
+    req.session.user = updatedUser;
+
+    // Redirect or render confirmation
+    res.redirect('/profile');
+  }catch(error){
+    console.error("Error updating profile:", error);
+    res.status(500).send("Something went wrong.");
+
+  }
+})
 app.post("/login",async(req,res)=>{
   const {email,password}=req.body;
-
+  
 try{
   const user = await User.findOne({ email:email.trim() });
 
